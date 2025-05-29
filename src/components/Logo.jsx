5f02e0aafedcc1263fbbe3320933080b75cf9588
Jose B Gomes <jbonigomes@yahoo.com.br>
2025-05-29 23:59:58 +0100

    Typo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default () => (
   <div className="flex justify-center">
-    <img src="/logo.png" alt="Logo" />
+    <img src="logo.png" alt="Logo" />
   </div>
 )
