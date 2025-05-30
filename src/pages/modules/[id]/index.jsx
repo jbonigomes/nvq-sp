2d2a6824ef7f11f5c7d27c563c028a67d98e168f
Jose B Gomes <jbonigomes@yahoo.com.br>
2025-05-30 17:59:31 +0100

    Add pdf make

--- src/pages/modules/[id]/index.jsx
+++ src/pages/modules/[id]/index.jsx
@@ -1,15 +1,47 @@
 import React from 'react'
+import pdfMake from 'pdfmake/build/pdfmake'
+import pdfFonts from 'pdfmake/build/vfs_fonts'
+
 import { Link, useParams } from 'react-router'
+
+pdfMake.addVirtualFileSystem(pdfFonts);
 
 export default () => {
   const { id } = useParams()
+
+  const dd = {
+    content: [
+      id,
+      'An example pdf',
+    ],
+  }
+
+  const onDownload = () => {
+    pdfMake.createPdf(dd).download(id)
+  }
+
+  const onOpen = () => {
+    pdfMake.createPdf(dd).open()
+  }
+
+  const onPrint = () => {
+    pdfMake.createPdf(dd).print()
+  }
 
   return (
     <>
       <h1>
         <Link to="/modules">&lt;</Link> Module {id}
       </h1>
-      <div className="fixed bottom-0 left-0 right-0 text-center">Export</div>
+      <button className="bg-white text-black p-3 m-3" onClick={onDownload}>
+        Download
+      </button>
+      <button className="bg-white text-black p-3 m-3" onClick={onOpen}>
+        Open
+      </button>
+      <button className="bg-white text-black p-3 m-3" onClick={onPrint}>
+        Print
+      </button>
     </>
   )
 }
